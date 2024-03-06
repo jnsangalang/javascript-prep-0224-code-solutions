58f19e48f372d2fe6fa23d115abdcb707e21f51c
Johan Sangalang <jnsangalang93@yahoo.com>
2024-03-06 18:39:54 +0000

    Edited into bracket notation.

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -12,6 +12,6 @@ person.job = 'disaster';
 console.log("This person's job is:", person.job);
 
 person['previousJob'] = 'Occupational Therapy Assistant';
-console.log("This person's previous job was:", person.previousJob);
+console.log("This person's previous job was:", person['previousJob']);
 
 console.log('All information of this person:', person);
