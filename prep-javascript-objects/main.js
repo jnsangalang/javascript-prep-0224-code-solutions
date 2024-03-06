06ec9c8dc71fa693e08e3f40490f6d4d069431e1
Johan Sangalang <jnsangalang93@yahoo.com>
2024-03-06 18:12:26 +0000

    Completed prep javascript objects.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,17 @@
+const person = {
+  firstName: 'Johan',
+  lastName: 'Sangalang',
+  hobby: 'Videogames',
+};
+console.log(person);
+
+const fullName = person.firstName + ' ' + person.lastName;
+console.log("This person's name is:", fullName);
+
+person.job = 'disaster';
+console.log("This person's job is:", person.job);
+
+person['previousJob'] = 'Occupational Therapy Assistant';
+console.log("This person's previous job was:", person.previousJob);
+
+console.log('All information of this person:', person);
