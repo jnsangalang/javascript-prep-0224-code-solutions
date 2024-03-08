bb9c0b04356913dc6c00af11d38b828b11349ecf
Johan Sangalang <jnsangalang93@yahoo.com>
2024-03-08 03:58:37 +0000

    Completed javascript arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,15 @@
+const language = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+
+console.log(language);
+
+const firstElement = language[0];
+console.log('The first element is:', firstElement);
+
+const thirdElement = language[2];
+console.log('The third element is:', thirdElement);
+
+const length = language.length;
+console.log('The length of this array is:', length);
+
+const lastElement = language[language.length - 1];
+console.log('The last element is:', lastElement);
